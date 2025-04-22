352753d74e91eba044ffb6e44d92e2b580999fde
Vaibhav <98206149+vaibhav-vai@users.noreply.github.com>
2025-04-22 19:12:21 +0530

    Added Day 24 - Mail Merge

---
+++ Day24_MailMerge/main.py
@@ -0,0 +1,12 @@
+PLACEHOLDER = "[name]"
+
+with open("./Input/Names/invited_names.txt") as names_file:
+    names = names_file.readlines()
+
+with open("./Input/Letters/starting_letter.txt") as letter_files:
+    letter_contents = letter_files.read()
+    for name in names:
+        stripped_name = name.strip()
+        new_letter = letter_contents.replace(PLACEHOLDER, stripped_name)
+        with open(f"./Output/ReadyToSend/letter_for_{stripped_name}.txt", mode="w") as completed_letters:
+            completed_letters.write(new_letter)
