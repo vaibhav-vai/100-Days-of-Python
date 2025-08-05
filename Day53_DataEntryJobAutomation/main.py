2c8cd11570746a87abc6264c4bf976017f250179
Vaibhav <98206149+vaibhav-vai@users.noreply.github.com>
2025-08-05 19:01:32 +0530

    Uploaded Day 53 - Data Entry Job Automation

---
+++ Day53_DataEntryJobAutomation/main.py
@@ -0,0 +1,54 @@
+from bs4 import BeautifulSoup
+import requests
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from time import sleep
+
+LINK = "https://docs.google.com/forms/d/e/1FAIpQLSf3cvesOtPc2DtwKTjFVboiNfRnqpASri82rJri6f2Rh6mNag/viewform?usp=sharing&ouid=117520298094426444346"
+
+headers = {
+    "User Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36",
+    "Accept Language": "en-US,en;q=0.9"
+}
+
+response = requests.get("https://appbrewery.github.io/Zillow-Clone/", headers)
+data = response.text
+
+soup = BeautifulSoup(data, "html.parser")
+
+all_links_element = soup.select(".StyledPropertyCardDataWrapper a")
+all_links = [link["href"] for link in all_links_element]
+print(f"There are {len(all_links)} links to individual listings in total: \n")
+print(all_links)
+
+all_address_elements = soup.select(".StyledPropertyCardDataWrapper address")
+all_addresses = [address.getText().replace("|", " ").strip() for address in all_address_elements]
+print(f"\n After having been cleaned up, the {len(all_addresses)} addresses now look like this: \n")
+print(all_addresses)
+
+all_price_elements = soup.select(".PropertyCardWrapper span")
+all_prices = [price.getText().replace("/mo", "").split("+")[0] for price in all_price_elements if "$" in price.text]
+print(f"\n After having been cleaned up, the {len(all_prices)} prices now look like this: \n")
+print(all_prices)
+
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_experimental_option("detach", True)
+driver = webdriver.Chrome(chrome_options)
+
+for n in range(len(all_links)):
+    driver.get(LINK)
+    sleep(2)
+
+    address = driver.find_element(By.XPATH,
+                                value='//*[@id="mG61Hd"]/div[2]/div/div[2]/div[1]/div/div/div[2]/div/div[1]/div/div[1]/input')
+    price = driver.find_element(By.XPATH,
+                                value='//*[@id="mG61Hd"]/div[2]/div/div[2]/div[2]/div/div/div[2]/div/div[1]/div/div[1]/input')
+    link = driver.find_element(By.XPATH,
+                            value='//*[@id="mG61Hd"]/div[2]/div/div[2]/div[3]/div/div/div[2]/div/div[1]/div/div[1]/input')
+    submit_button = driver.find_element(By.XPATH,
+                                value='//*[@id="mG61Hd"]/div[2]/div/div[3]/div[1]/div[1]/div/span/span')
+
+    address.send_keys(all_addresses[n])
+    price.send_keys(all_prices[n])
+    link.send_keys(all_links[n])
+    submit_button.click()
