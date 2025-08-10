ca7ab26cb13f91e72b8c8e38dfb5ce21b506c0a4
Vaibhav <98206149+vaibhav-vai@users.noreply.github.com>
2025-08-10 19:48:23 +0530

    Create main.py

---
+++ Day60_BlogWithContactForm/main.py
@@ -0,0 +1,56 @@
+from flask import Flask, render_template, request
+import requests
+import smtplib
+
+EMAIL = "YOUR_EMAIL"
+PASSWORD = "YOUR_PASSWORD"
+
+posts = requests.get("https://api.npoint.io/c790b4d5cab58020d391").json()
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def get_all_posts():
+    return render_template("index.html", all_posts=posts)
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/contact", methods=["Get", "POST"])
+def contact():
+    if request.method == "POST":
+        data = request.form
+        print(data["name"])
+        print(data["email"])
+        print(data["phone"])
+        print(data["message"])
+        send_email(data["name"], data["email"], data["phone"], data["message"])
+        return render_template("contact.html", msg_sent=True)
+    return render_template("contact.html", msg_send=False)
+
+
+@app.route("/post/<int:index>")
+def show_post(index):
+    requested_post = None
+    for blog_post in posts:
+        if blog_post["id"] == index:
+            requested_post = blog_post
+    return render_template("post.html", post=requested_post)
+
+def send_email(name, email, phone, message):
+    email_message = f"Subject: New Message fromBlog\n\nName: {name}\nEmail: {email}\nPhone: {phone}\nMessage: {message}"
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(user=EMAIL, password=PASSWORD)
+        connection.sendmail(
+            from_addr = EMAIL,
+            to_addrs = EMAIL,
+            msg = email_message.encode("utf-8")
+        )
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5001)
